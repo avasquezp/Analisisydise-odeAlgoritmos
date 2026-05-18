72a0d11523edd68f4e23e445208581b3f205d139
avasquezp <avasquezp@ulasalle.edu.pe>
2026-05-18 18:58:15 -0500

    tercera modificacion

--- Examen Parcial/Problema 1/codigo1.java
+++ Examen Parcial/Problema 1/codigo1.java
@@ -1,14 +1,16 @@
 public class Problema1 {
-    
-    // en esta parte utiliza ek metodo recursivo que va a verificar si el arreglo esta bien ordenado de menor hasta el mayor 
-    public static boolean orden(int[] arr, int inicio) {
 
-        // si es que queda un elemento va a estar ordenado, es como si fuera ir cortando todo el array por la derecha 
-        if (tam == 1) return true;
-        // si es que el ultimo elemento esta menor que el anterior no va a estar ordenado 
-        if (arr[tam - 1] < arr[tam - 2]) return false;
-        // aca va a ver si el arreglo esta reducido en 1 
-        return orden(arr, tam - 1);
+    // en esta parte utiliza ek metodo recursivo que va a verificar si el arreglo esta bien ordenado de menor hasta el mayor 
+    public static boolean orden(int[] array, int izquierda, int derecha) {
+
+        // el caso base es que los indices se cruzaron o son iguales es que esta ordenado 
+        if (izquierda >= derecha) return true;
+        // si el extremo que esta a la izquierda es mayor que el derecho significa que no esta ordenado 
+        if (array[izquierda] > array[derecha]) return false;
+        // si es que el par izquierdo no esta bien ordenado va a retornar un false
+        if (array[izquierda] > array[izquierda + 1]) return false;
+        // se va a achicar por ambos lados
+        return orden(array, izquierda + 1, derecha - 1);
     }
 
     public static void main(String[] args) {
@@ -18,11 +20,11 @@ public class Problema1 {
         // muestra el array 1 y su resultado si es true o false en el compilador 
         System.out.print("array 1: ");
         for (int n : array1) System.out.print(n + " ");
-        System.out.println(" = " + orden(array1, 0));
+        System.out.println(" = " + orden(array1, 0, array1.length - 1));
 
         // lo mismo que el array 1
         System.out.print("array 2: ");
         for (int n : array2) System.out.print(n + " ");
-        System.out.println(" = " + orden(array2, 0));
+        System.out.println(" = " + orden(array2, 0, array2.length - 1));
     }
 }
