0757ca8867916073c0f39837504243d246799494
avasquezp <avasquezp@ulasalle.edu.pe>
2026-05-18 19:36:59 -0500

    cuarta modificacion

--- Examen Parcial/Problema 1/codigo1.java
+++ Examen Parcial/Problema 1/codigo1.java
@@ -1,30 +1,31 @@
-public class Problema1 {
+import java.util.Scanner;
 
+public class Problema1 {
     // en esta parte utiliza ek metodo recursivo que va a verificar si el arreglo esta bien ordenado de menor hasta el mayor 
     public static boolean orden(int[] array, int izquierda, int derecha) {
-
-        // el caso base es que los indices se cruzaron o son iguales es que esta ordenado 
+        // el caso base es que los indices se cruzaron o son iguales es que esta ordenado
         if (izquierda >= derecha) return true;
-        // si el extremo que esta a la izquierda es mayor que el derecho significa que no esta ordenado 
+         // si el extremo que esta a la izquierda es mayor que el derecho significa que no esta ordenado 
         if (array[izquierda] > array[derecha]) return false;
-        // si es que el par izquierdo no esta bien ordenado va a retornar un false
+         // si es que el par izquierdo no esta bien ordenado va a retornar un false
         if (array[izquierda] > array[izquierda + 1]) return false;
         // se va a achicar por ambos lados
         return orden(array, izquierda + 1, derecha - 1);
     }
 
     public static void main(String[] args) {
-        int[] array1 = {1, 2, 3, 5, 8};
-        int[] array2 = {1, 4, 2, 8};
+        Scanner sc = new Scanner(System.in);
 
-        // muestra el array 1 y su resultado si es true o false en el compilador 
-        System.out.print("array 1: ");
-        for (int n : array1) System.out.print(n + " ");
-        System.out.println(" = " + orden(array1, 0, array1.length - 1));
+        System.out.print("cuantos numeros: ");
+        int tam = sc.nextInt();
+        int[] array = new int[tam];
 
-        // lo mismo que el array 1
-        System.out.print("array 2: ");
-        for (int n : array2) System.out.print(n + " ");
-        System.out.println(" = " + orden(array2, 0, array2.length - 1));
+        for (int i = 0; i < tam; i++) {
+            System.out.print("numero " + (i + 1) + ": ");
+            array[i] = sc.nextInt();
+        }
+
+        System.out.println(orden(array, 0, array.length - 1));
+        sc.close();
     }
 }
