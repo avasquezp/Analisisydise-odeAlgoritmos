71b7e4d70c439d5318f0425777a03c86ea88b17f
avasquezp <avasquezp@ulasalle.edu.pe>
2026-06-25 01:51:41 -0500

    commit ultimo

--- Actividad 9. Programación Dinámica/Cambio mínimo de monedas.cpp
+++ Actividad 9. Programación Dinámica/Cambio mínimo de monedas.cpp
@@ -2,40 +2,62 @@
 using namespace std;
 
 int main() {
+
     int monedas[] = {1, 2, 5};
-    int n = 11;
+    int n = 3;
     int monto;
     cout << "ingresa el monto: ";
     cin >> monto;
 
-    // dp[i] sera el minimo de monedas para formar el monto i
-    int dp[100];
-
-    // en el caso base para formar 0 se necesita 0 monedas 
-    dp[0] = 0;
-
-    // inicializamos todo con 9999 que significa imposible por ahora
-    for (int i = 1; i <= monto; i++) {
-        dp[i] = 9999;
+    // si el usuario ingresa un monto invalido, avisamos y salimos
+    if (monto <= 0) {
+        cout << "el monto debe ser mayor a 0." << endl;
+        return 1;
     }
-
-    // para cada monto i probamos cada moneda y nos quedamos con el minimo
+    //aca va la tabla DP 
+    int dp[100];
+    int desde[100];
+    dp[0] = 0;
+    desde[0] = -1;
+    for (int i = 1; i <= monto; i++) {
+        dp[i]    = 9999;
+        desde[i] = -1;
+    }
+    // relacion de recurrencia 
     for (int i = 1; i <= monto; i++) {
         for (int j = 0; j < n; j++) {
             if (monedas[j] <= i) {
-                dp[i] = min(dp[i], dp[i - monedas[j]] + 1);
+                int opcion = dp[i - monedas[j]] + 1;
+                if (opcion < dp[i]) {
+                    dp[i]    = opcion;
+                    desde[i] = monedas[j];
+                }
             }
         }
     }
-
-    cout << "tabla DP: [";
+    cout << "\ntabla DP: [";
     for (int i = 0; i <= monto; i++) {
         cout << dp[i];
-        if (i < monto) cout << ", ";
+        if (i < monto) cout << ",";
     }
     cout << "]" << endl;
 
-    cout << "minimo de monedas: " << dp[monto] << endl;
+    if (dp[monto] == 9999) {
+        cout << "no hay solucion con las monedas disponibles." << endl;
+    } else {
+        cout << "cantidad minima de monedas: " << dp[monto] << endl;
+
+        cout << "combinacion: ";
+        int actual   = monto;
+        bool primero = true;
+        while (actual > 0) {
+            if (!primero) cout << " + ";
+            cout << desde[actual];
+            actual  = actual - desde[actual];
+            primero = false;
+        }
+        cout << endl;
+    }
 
     return 0;
 }
