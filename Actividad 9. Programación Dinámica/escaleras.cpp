44842d675f4175ba4b8cdd3e9babb99d3d3a544d
avasquezp <avasquezp@ulasalle.edu.pe>
2026-06-24 23:29:34 -0500

    modificacion2

--- Actividad 9. Programación Dinámica/escaleras.cpp
+++ Actividad 9. Programación Dinámica/escaleras.cpp
@@ -1,32 +1,31 @@
 #include <iostream>
 using namespace std;
 
+// memo[i] guarda el resultado para no repetirlo
+int memo[100];
+
+int escaleras(int n) {
+    // si ya llegamos al inicio o al escalon 1 hay  1 forma
+    if (n == 0 || n == 1) return 1;
+    // aca ya calculamos este escalon, devolvemos el resultado 
+    if (memo[n] != -1) return memo[n];
+    memo[n] = escaleras(n-1) + escaleras(n-2);
+    return memo[n];
+}
+
 int main() {
     int n;
     cout << "entrada: ";
     cin >> n;
-    // dp: dp[i] es igual al numero de formas distintas de llegar al escalon i
-    int dp[100];
-
-    // dp[0] = 1 hay una forma de estar en el inicio y no moverse
-    // dp[1] = 1 solo existe un camino posible
-    dp[0] = 1;
-    dp[1] = 1;
-
-    // Para llegar al escalon i puedo venir de i-1 que seria 1 paso o de i-2 que son 2 pasos, sumamos ambas posibilidades porque queremos contar formas
-    for (int i = 2; i <= n; i++) {
-        dp[i] = dp[i-1] + dp[i-2];
-    }
-
-    // Mostramos cada dp[i] para ver como se construyo la solucion de abajo hacia arriba
+    // inicializamos toda la tabla en -1 
+    for (int i = 0; i <= n; i++) memo[i] = -1;
     cout << "tabla DP: [";
     for (int i = 0; i <= n; i++) {
-        cout << dp[i];
+        cout << escaleras(i);
         if (i < n) cout << ", ";
     }
     cout << "]" << endl;
-
-    cout << "formas posibles: " << dp[n] << endl;
+    cout << "formas posibles: " << escaleras(n) << endl;
 
     return 0;
 }
