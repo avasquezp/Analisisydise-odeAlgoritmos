6cd760eafacf7d4a8437d303b2ec0cf6be03e945
avasquezp <avasquezp@ulasalle.edu.pe>
2026-06-25 01:23:15 -0500

    Update Cambio mínimo de monedas.cpp

--- Actividad 9. Programación Dinámica/Cambio mínimo de monedas.cpp
+++ Actividad 9. Programación Dinámica/Cambio mínimo de monedas.cpp
@@ -2,21 +2,24 @@
 using namespace std;
 
 int main() {
-    int monedas[] = {1, 3, 4};
-    int n = 3; // cantidad de monedas
+    int monedas[] = {1, 2, 5};
+    int n = 11;
     int monto;
     cout << "ingresa el monto: ";
     cin >> monto;
-    // dp[i] el minimo de monedas para formar el monto i
+
+    // dp[i] sera el minimo de monedas para formar el monto i
     int dp[100];
-    // en el caso base para formar 0 se necesitan 0 monedas
+
+    // en el caso base para formar 0 se necesita 0 monedas 
     dp[0] = 0;
-    // inicializamos todo con un numero muy grande 
+
+    // inicializamos todo con 9999 que significa imposible por ahora
     for (int i = 1; i <= monto; i++) {
         dp[i] = 9999;
     }
 
-    // para cada monto i, probamos cada moneda y nos quedamos con el minimo
+    // para cada monto i probamos cada moneda y nos quedamos con el minimo
     for (int i = 1; i <= monto; i++) {
         for (int j = 0; j < n; j++) {
             if (monedas[j] <= i) {
@@ -24,13 +27,14 @@ int main() {
             }
         }
     }
-    // mostramos la tabla
+
     cout << "tabla DP: [";
     for (int i = 0; i <= monto; i++) {
         cout << dp[i];
         if (i < monto) cout << ", ";
     }
     cout << "]" << endl;
+
     cout << "minimo de monedas: " << dp[monto] << endl;
 
     return 0;
